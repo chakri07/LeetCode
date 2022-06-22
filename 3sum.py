6effcb99026794cb251475aba12f6eee825658c7
chakradhar <chakradhar@uchicago.edu>
2022-06-22 08:06:46 -0500

    just before amazon prep

--- 3sum.py
+++ 3sum.py
@@ -1,22 +1,3 @@
-def threeSum(self, nums: list[int]) -> list[list[int]]:
-    nums = sorted(nums)
-    ans = []
-    for i in range(0, len(nums)-2):
-        left = i + 1
-        right = len(nums)-1
-        while( left < right):
-            sum = nums[i] + nums[left] + nums[right]
-            if sum == 0 :
-                sortedArray = sorted([nums[i],nums[left],nums[right]])
-                if sortedArray not in ans:
-                    ans.append(sortedArray)
-                left = left +1 
-            elif sum > 0 : 
-                right = right - 1
-            else :
-                left = left + 1
-    return ans
-
 '''
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
@@ -37,3 +18,23 @@ Output: []
 
 link : https://leetcode.com/problems/3sum/
 '''
+
+def threeSum(self, nums: list[int]) -> list[list[int]]:
+    nums = sorted(nums)
+    ans = []
+    for i in range(0, len(nums)-2):
+        left = i + 1
+        right = len(nums)-1
+        while( left < right):
+            sum = nums[i] + nums[left] + nums[right]
+            if sum == 0 :
+                sortedArray = sorted([nums[i],nums[left],nums[right]])
+                if sortedArray not in ans:
+                    ans.append(sortedArray)
+                left = left +1 
+            elif sum > 0 : 
+                right = right - 1
+            else :
+                left = left + 1
+    return ans
+
