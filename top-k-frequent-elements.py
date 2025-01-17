25ae0afbb3435c64cb635ceff8ecd6b188ecccfb
Chakradhar Gummidela <36010138+chakri07@users.noreply.github.com>
2025-01-17 05:06:38 +0000

    better solution

--- top-k-frequent-elements.py
+++ top-k-frequent-elements.py
@@ -14,6 +14,32 @@ Output: [1]
 
 '''
 
+# using sorting 
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_map = {}
+
+        for num in nums:
+            if num in freq_map: 
+                freq_map[num] +=1
+            else:
+                freq_map[num] = 1
+
+        # now sort the frequ_map 
+
+        sorted_freq_map = dict(sorted(freq_map.items(),
+            key = lambda item: item[1],
+            reverse = True))
+
+        print(sorted_freq_map)
+
+        return list(sorted_freq_map.keys())[:k]
+
+
+# solution 2
+
+
 import collections
 import heapq
 from typing import List
