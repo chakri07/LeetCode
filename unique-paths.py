f35c533d760c07d4972a5243986282fad2f913ff
Chakradhar Gummidela <chakrig@amazon.com>
2025-01-18 13:03:03 -0800

    updated new solution

--- unique-paths.py
+++ unique-paths.py
@@ -45,3 +45,29 @@ class Solution:
                     dp[x][y] = dp[x-1][y]
         
         return dp[m-1][n-1]
+    
+
+## Solution 2 : prepopulate the edges
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[ 0 for _ in range(n)] for _ in range(m)]
+
+        grid[0][0] = 1
+        ## Pre populating the edge rows
+        for i in range(m):
+            grid[i][0] = 1
+        for i in range(n):
+            grid[0][i] = 1
+
+        for row in range(1,m):
+            for col in range(1,n):
+                cell_val = 0 
+                if row -1 >= 0:
+                    cell_val = cell_val + grid[row -1][col]
+                if col-1 >= 0:
+                    cell_val = cell_val + grid[row][col-1]
+
+                grid[row][col] = cell_val
+
+        return grid[m-1][n-1]
