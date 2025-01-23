bee5261034426ed862130656f1f8386d5be992e5
Chakradhar Gummidela <chakrig@amazon.com>
2025-01-22 19:57:36 -0800

    LIS

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -24,16 +24,30 @@ Output: 1
 Link: https://leetcode.com/problems/longest-increasing-subsequence/
 '''
 
+## Bottom up dp
 class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
         dp = [1] * len(nums)
         dp[0] = 1
-        ans = 0 
+        
         for i in range(1,len(nums)):
             for j in range(0,i):
                 if nums[j]< nums[i]:
                     dp[i] = max(dp[i],dp[j]+1)
-                    ans = max(ans,dp[i])
-        
-        return ans
+                
+        return max(dp)
             
+
+## Top down dp
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        dp[-1] = 1
+
+        for i in range(len(nums) -1,-1,-1):
+            for j in range(i+1,len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[j]+1,dp[i])
+
+        return max(dp)
+
