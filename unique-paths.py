1f8375efb27fe08907c1f66c9a023d587ee06501
Chakradhar Gummidela <chakrig@amazon.com>
2025-02-09 14:17:59 -0800

    micro update

--- unique-paths.py
+++ unique-paths.py
@@ -62,12 +62,10 @@ class Solution:
 
         for row in range(1,m):
             for col in range(1,n):
-                cell_val = 0 
                 if row -1 >= 0:
-                    cell_val = cell_val + grid[row -1][col]
+                    grid[row][col] = grid[row][col] + grid[row -1][col]
                 if col-1 >= 0:
-                    cell_val = cell_val + grid[row][col-1]
+                    grid[row][col] = grid[row][col] + grid[row][col-1]
 
-                grid[row][col] = cell_val
 
         return grid[m-1][n-1]
