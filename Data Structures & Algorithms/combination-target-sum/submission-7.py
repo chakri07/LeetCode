ac2f2ceadf3be6c1a2ad7c818d190a5714fde546
Chakradhar Gummidela <36010138+chakri07@users.noreply.github.com>
2026-06-14 23:04:41 -0700

    Add: combination-target-sum - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        output = []
+        self.helper(0,nums,target,[],output)
+
+        return output
+
+    def helper(self,idx,nums,target,curr,output):
+        if target == 0:
+            output.append(curr[:])
+            return
+        # Base Case 2: Failure (Target went negative or out of bounds)
+        if target < 0 or idx >= len(nums):
+            return 
+        
+        for i in range(idx, len(nums)):
+            curr.append(nums[i])
+            self.helper(i, nums, target - nums[i], curr,output)
+            curr.pop()
+
+            
+    
+
+        
